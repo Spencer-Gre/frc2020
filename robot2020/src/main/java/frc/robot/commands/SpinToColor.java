eb29474b68dad0c9468ef301f0eb35c592323171
Spencer Greene <aklixio@gmail.com>
2020-02-16 15:43:44 -0500

    SpinToColor is not fixed

--- robot2020/src/main/java/frc/robot/commands/SpinToColor.java
+++ robot2020/src/main/java/frc/robot/commands/SpinToColor.java
@@ -7,6 +7,9 @@
 
 package frc.robot.commands;
 
+import java.io.Console;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ColorWheel;
 
@@ -24,16 +27,25 @@ public class SpinToColor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    SmartDashboard.putBoolean("Is met?", false);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(m_colorWheel.getRequired().length() > 0){
-      Boolean isMet = false;
-      if(m_colorWheel.getColor() == m_colorWheel.getRequired()) {
-        isMet = true;
-      }
+    Boolean isMet = false;
+    SmartDashboard.putBoolean("Is met?", isMet);
+    
+    if(m_colorWheel.getRequired().length() > 0) {
+      while(isMet = false){
+        var color = m_colorWheel.getColor();
+        if (color == m_colorWheel.getRequired()) {
+          isMet = true;
+        }
+    }
+
+    SmartDashboard.putBoolean("Is met?", isMet);
+    this.end(false);
     }
   }
 
